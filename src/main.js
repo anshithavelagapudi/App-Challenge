0cf88eb10242e5f9b8eea5c0bea5ec1877ba3018
Anshitha <anshithavelagapudi@gmail.com>
2020-03-02 11:39:22 -0600

    edited in index and main files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+document.addEventListener('click', event => {
+        if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
+      });
+
 function multiplyBy()
 {
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        console.log(num1);
-        console.log(num2);
-        document.getElementById("result").innerHTML = num1 * num2;
+        item = document.getElementById("firstNumber").value;
+        sizeEach = document.getElementById("secondNumber").value;
+        console.log(item);
+        console.log(sizeEach);
+        document.getElementById("result").innerHTML = item * sizeEach;
 }
+
