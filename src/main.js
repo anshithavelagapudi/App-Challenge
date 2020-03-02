f0252c5552da7a36a77bfb9c5a563ccc2a6c9765
Anshitha <anshithavelagapudi@gmail.com>
2020-03-02 11:27:36 -0600

    changes made

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ function multiplyBy()
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
+        console.log(num1);
+        console.log(num2);
         document.getElementById("result").innerHTML = num1 * num2;
 }
